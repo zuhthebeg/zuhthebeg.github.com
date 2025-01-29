aa7429cf209db4f2003356c6073f98ca36071403
cocy <zuhthebeg@naver.com>
2025-01-29 23:35:49 +0900

    mod

---
+++ blockpuzzle/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'block-puzzle-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+}); 
